cd03e9427792acf08c01a1a536521689ec73161a
Zsolt Harmath <harmathzs@gmail.com>
2026-05-28 09:06:12 +0200

    fix: update Kings component state structure and improve rendering of king data

--- react-app/src/components/Kings.jsx
+++ react-app/src/components/Kings.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 
 export default function Kings(props) {
-    const [kings, setKings] = useState([])
+    const [kings, setKings] = useState({data:[]})
 
     useEffect(()=>{
         fetch('https://fly-starter-1-backend.fly.dev/kings')
@@ -11,8 +11,14 @@ export default function Kings(props) {
     }, [])
 
     return <>
-        <p>
-            {JSON.stringify(kings)}
-        </p>
+        <h2>Kings</h2>
+        {kings.data.map((king, idx)=><article key={idx}>
+            <hr />
+            <ul>
+                <li>index: {idx}</li>
+                <li>Name: {king.name} {king.number}</li>
+                <li>Country: {king.country}</li>
+            </ul>
+        </article>)}
     </>
 }
